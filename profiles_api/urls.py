a1d9795324725d626bab652223a621e9c3ff0926
shubahm lengare <shubhamlengare09>
2023-12-22 21:55:07 +0530

    added user profile feed API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,6 +7,7 @@ from profiles_api import views
 router =DefaultRouter()
 router.register('hello-viewSet',views.HelloViewSet, base_name='hello-viewset')
 router.register('profile',views.UserProfileViewSet)
+router.register('feed',views.UserProfileFeedViewSet)
 
 
 
