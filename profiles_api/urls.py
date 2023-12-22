452dfbaa05972de957adadc0fa65d6db4be28e21
shubahm lengare <shubhamlengare09>
2023-12-22 18:10:20 +0530

    added login api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,7 +8,10 @@ router =DefaultRouter()
 router.register('hello-viewSet',views.HelloViewSet, base_name='hello-viewset')
 router.register('profile',views.UserProfileViewSet)
 
+
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('',include(router.urls))
 ]
